99144e9e3010fac035228cd03d5946f33b729855
JhDroid <lorzx77@gmail.com>
2021-03-02 12:19:23 +0900

    jitpack 적용 후 example 라이브러리 추가

--- app/src/main/java/com/jhdroid/jitpack/example/MainActivity.kt
+++ app/src/main/java/com/jhdroid/jitpack/example/MainActivity.kt
@@ -2,10 +2,15 @@ package com.jhdroid.jitpack.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jhdroid.jitpack.exlibrary.Example
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val example = Example()
+        example.getString()
+        example.getBlogUrl()
     }
 }
